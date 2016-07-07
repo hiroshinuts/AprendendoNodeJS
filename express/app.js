7378858f15327e9580f5bc1b52896f5ecf850e5b
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-07-06 23:40:34 -0300

    Video 25/26 , criando algumas rotas e umas telas

--- express/app.js
+++ express/app.js
@@ -7,8 +7,8 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 var load         = require('express-load');
 
-var routes = require('./routes/index');
-var users  = require('./routes/users');
+//var routes = require('./routes/index');
+//var users  = require('./routes/users');
 
 var app = express();
 
@@ -27,12 +27,15 @@ app.use(cookieParser());
 app.use(session({ secret: '123' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+
+load('models').then('controllers').then('routes').into(app);
 
 //middleware
-app.use(erros.notfound);
-app.use(erros.serverError);
+//app.use(erros.notfound);
+//app.use(erros.serverError);
+
 
 app.listen(3000, function() {
     console.log('Express server listening on port 3000');
